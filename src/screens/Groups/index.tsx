0cb57b48c2576f743cff5f07270fbc984f69d323
Lucas Mattos <lcsmattos7@gmail.com>
2023-09-14 21:23:08 -0300

    Atualizações e criação de localstorage

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { FlatList } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
 
 import Button from "@components/Button";
 import Header from "@components/Header";
@@ -8,15 +8,34 @@ import Highlight from "@components/Highlight";
 import EmptyList from "@components/EmptyList";
 import GroupCard from "@components/GroupCard";
 
+import { getAllGroups } from "@storage/groups/groupsGetAll";
+
 import { Container } from "./styles";
 
 export default function Groups() {
-  const [groups, setGroups] = useState<string[]>(["bLA", "bla"]);
+  const [groups, setGroups] = useState<string[]>([]);
 
   const navigation = useNavigation();
 
+  useFocusEffect(
+    useCallback(() => {
+      fetchGroups();
+    }, [])
+  );
+
   function handleNewGroup() {
     navigation.navigate("new");
+  }
+
+  async function fetchGroups() {
+    try {
+      const data = await getAllGroups();
+
+      setGroups(data);
+      return;
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
@@ -28,10 +47,10 @@ export default function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={() => (
           <EmptyList message="Ninguem por aqui ainda, inicie a primeira turma ;)" />
         )}
-        contentContainerStyle={groups.length === 0 && { flex: 1 }}
       />
       <Button title="Nova Turma" onPress={handleNewGroup} />
     </Container>
--- src/screens/NewGroups/index.tsx
+++ src/screens/NewGroups/index.tsx
@@ -7,14 +7,29 @@ import Header from "@components/Header";
 import Highlight from "@components/Highlight";
 
 import { Content, Container, Icon } from "./styles";
+import { groupCreate } from "@storage/groups/groupCreate";
+import { AppError } from "@utils/AppError";
+import { Alert } from "react-native";
 
 export default function NewGroups() {
   const [teamName, setTeamName] = useState<string>("");
 
   const navigate = useNavigation();
 
-  function handleTeams() {
-    navigate.navigate("teams", { group: teamName });
+  async function handleTeams() {
+    try {
+      if (teamName.length === 0) {
+        return Alert.alert("Novo Grupo", "Informe o nome da turma.");
+      }
+      await groupCreate(teamName);
+      navigate.navigate("teams", { group: teamName });
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert("Novo Grupo", error.message);
+      } else {
+        Alert.alert("Novo Grupo", "Não foi possivel criar um novo grupo.");
+      }
+    }
   }
 
   return (
@@ -29,7 +44,11 @@ export default function NewGroups() {
           subTitle="crie uma turma para adicionar pessoas"
         />
 
-        <Input placeholder="Nome da Turma" onChangeText={setTeamName} />
+        <Input
+          placeholder="Nome da Turma"
+          onChangeText={setTeamName}
+          autoCorrect={false}
+        />
 
         <Button title="Criar" style={{ marginTop: 10 }} onPress={handleTeams} />
       </Content>
